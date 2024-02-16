14f39a8d1f859cccd86109203dae3674413a7362
Bmoore4452 <r.moore803@gmail.com>
2024-02-16 18:17:43 -0500

    completed logic for multiple choice

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import random
+
 # Create level 1 questions and answers
 currentRavens = [
     {
         "question": "What jersey number does Lamar Jackson wear? ",
-        "answers": ["8", "5"],
+        "answers": ["8", "5", "1", "15"],
         "correct": "8",
     },
-    # ("What position does Lamar Jackson play? ", "QB"),
-    # ("Who is the head coach for the Baltimore Ravens? ", "John Harbough"),
-    # (
-    #     "What is the primary color of the Baltimore Ravens? (other than black) ",
-    #     "Purple",
-    # ),
-    # ("How many wins did the 2023 Baltimore Ravens have? ", "13"),
+    {
+        "question": "What position does Lamar Jackson play? ",
+        "answers": ["Quarterback", "Running Back", "Wide Reciever", "Linebacker"],
+        "correct": "Quarterback",
+    },
+    {
+        "question": "Who is the head coach for the Baltimore Ravens? ",
+        "answers": ["John Harbough", "Andy Reid", "Bill Belichick", "Nick Sirianni"],
+        "correct": "John Harbough",
+    },
+    {
+        "question": "What is the primary color of the Baltimore Ravens? (other than black) ",
+        "answers": ["Purple", "Yellow", "Red", "Green"],
+        "correct": "Purple",
+    },
+    {
+        "question": "How many wins did the 2023 Baltimore Ravens have? ",
+        "answers": ["13", "6", "11", "9"],
+        "correct": "13",
+    },
 ]
 
 # Create level 2 questions and answers
@@ -44,36 +59,87 @@ legendRavens = [
 
 # Start the game
 def main():
+    score = 0
+
+    # Define multiple choice input options
     choices = ["a", "b", "c", "d"]
+    
+    # Shuffle questions and answers so not test is ever the same
+    random.shuffle(currentRavens)
+    random.shuffle(choices)
+    
+    # Iterate through questions list
     for x in currentRavens:
+
+        # Iterate through and identify each question, answers, and correct answer
         for y in x:
+
+            # Identify question
             if y == "question":
                 question = x[y]
-            if y == "answers":
-                a, b = x[y]
-            if y == "correct":
-                correct = x[y]
-                print(question)
-                print(f"A. {a}")
-                print(f"B. {b}")
-            choice = input("Pick: ").casefold()
 
-    # result1 = askCurrent(currentRavens)
-    # if result1 >= 60:
-    #     result2 = askRecent(recentRavens)
-    # else:
-    #     print("Please study the current Baltimore Ravens")
-    #     return
-    # if result2 >= 60:
-    #     result3 = askLegend(legendRavens)
-    # else:
-    #     print("Please study the 2013 Baltimore Ravens")
-    #     return
-    # if result3 >= 60:
-    #     print("You are a true Baltimore Ravens fan")
-    # else:
-    #     print("Please study the 2000 Baltimore Ravens")
-    #     return
+            # Identify answer options
+            if y == "answers":
+                a = x[y]
+                random.shuffle(a)
+
+            # Identify correct answer
+            if y == "correct":
+                correct = {"correct": x[y]}
+
+                # Prints each question
+                print(question)
+
+                # Converts input options and answer options into a dictionary
+                res = {choices[i]: a[i] for i in range(len(choices))}
+
+                # Prints each options for user to choose from
+                print(f"A. {res['a']}")
+                print(f"B. {res['b']}")
+                print(f"C. {res['c']}")
+                print(f"D. {res['d']}")
+
+                # Prompt user input
+                while True:
+                    choice = input("Pick: ").casefold()
+
+                    # Check for a valid choice
+                    if choice in choices:
+
+                        # Check for correct input
+                        if res[choice] == correct["correct"]:
+                            score += 1
+                            break
+                        else:
+                            print("Incorrect")
+                            break
+                    else:
+                        print("***Please choose a valid answer (a, b, c , or d)***")
+
+    # Evaluate user's performance
+    result = score / len(currentRavens) * 100
+    if result >= 60:
+        print("You have completed level 1")
+    else:
+        print("You are not a Baltimore Ravens fan")
+
+
+# result1 = askCurrent(currentRavens)
+# if result1 >= 60:
+#     result2 = askRecent(recentRavens)
+# else:
+#     print("Please study the current Baltimore Ravens")
+#     return
+# if result2 >= 60:
+#     result3 = askLegend(legendRavens)
+# else:
+#     print("Please study the 2013 Baltimore Ravens")
+#     return
+# if result3 >= 60:
+#     print("You are a true Baltimore Ravens fan")
+# else:
+#     print("Please study the 2000 Baltimore Ravens")
+#     return
 
 
 # Ask questions for level 1
